3220c074ad6c1b5fd45596788c9399d54e8277f5
gitduck6 <warmsot19@gmail.com>
2026-05-05 18:08:44 +0400

    Added dynamic token size, but kinda janky

--- main.c
+++ main.c
@@ -8,6 +8,9 @@
     * instead of using the system() function (which relies on other shells such as ksh and dash from what i know)
     * rightnow my goal is to implement a simple sh clone, with pipes, redirects and everything
     * but who knows how far it will go
+    * 
+    * Currently does almost nothing but execute commands in a very rudimentary way
+    * did i mention that im making this cos i was too stupid to read xv6's sh.c
 */
 
 char * readline(FILE * fd)
@@ -43,8 +46,12 @@ char * readline(FILE * fd)
 char ** tokenize(char *string)
 {
     size_t token_count = 0;
-    size_t token_buffer = 4;
-    char ** token_array = malloc(sizeof(char *) * token_buffer);
+    size_t token_size = 2;
+    char ** token_array = malloc(sizeof(char *) * token_size);
+    if (token_array == NULL) 
+    {
+        return NULL;
+    } 
 
     char * token = strtok(string," \t"); // either space or tab
     while (token != NULL)
@@ -52,7 +59,27 @@ char ** tokenize(char *string)
         token_array[token_count] = token;
         token = strtok(NULL," \t");
         token_count++;
+
+        if (token_count >= token_size)
+        {
+            token_size *= 4;
+            char **temp = realloc(token_array,sizeof(char*) * (token_size + 1));
+            if (temp == NULL) 
+            {
+                free(token_array);
+                return NULL;
+            } 
+            else token_array = temp;
+        }
     }
+
+    char ** temp = realloc(token_array,sizeof(char*) * token_size );
+    if (temp == NULL) 
+    {
+        free(token_array);
+        return NULL;
+    } 
+    else token_array = temp;
 
     token_array[token_count] = NULL;
     return token_array;
