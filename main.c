cd0865a9852b4691afa1c86897c78a209a6e10dd
gitduck6 <warmsot19@gmail.com>
2026-05-05 17:51:36 +0400

    made simple execution logic

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <unistd.h>
 /*
     * Hello im really bad at this stuff so ima try to make a new larger project
     * it is a basic shell BUT this time it will be more standalone
-    * instead of using the system() function (which relies on other shells such as sh and dash)
+    * instead of using the system() function (which relies on other shells such as ksh and dash from what i know)
     * rightnow my goal is to implement a simple sh clone, with pipes, redirects and everything
     * but who knows how far it will go
 */
@@ -36,8 +37,31 @@ char * readline(FILE * fd)
     return dest;
 }
 
+/*
+    attempts to return a cstring array in the {"Hello", "World", "!", NULL}; format
+*/
+char ** tokenize(char *string)
+{
+    size_t token_count = 0;
+    size_t token_buffer = 4;
+    char ** token_array = malloc(sizeof(char *) * token_buffer);
+
+    char * token = strtok(string," \t"); // either space or tab
+    while (token != NULL)
+    {
+        token_array[token_count] = token;
+        token = strtok(NULL," \t");
+        token_count++;
+    }
+
+    token_array[token_count] = NULL;
+    return token_array;
+}
+
 int main(void)
 {
     char * code = readline(stdin);
-    printf("%s ",code);
+    execv("/usr/bin/ls", tokenize(code));
+    free(code);
+    return 0;
 }
