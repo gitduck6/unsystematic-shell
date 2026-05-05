6289fa39384e697539ecc599a55e3c1fd44d3793
gitduck6 <warmsot19@gmail.com>
2026-05-05 16:36:37 +0400

    Added some comments

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    * Hello im really bad at this stuff so ima try to make a new larger project
+    * it is a basic shell BUT this time it will be more standalone
+    * instead of using the system() function (which relies on other shells such as sh and dash)
+    * rightnow my goal is to implement a simple sh clone, with pipes, redirects and everything
+    * but who knows how far it will go
+*/
+
 char * readline(FILE * fd)
 {
     size_t size = 16;
