5cfcbff3e63ac282d1a2e7f0fc8f65068a32bd32
gitduck6 <warmsot19@gmail.com>
2026-05-08 15:16:32 +0400

    Using the new struct

--- src/lexer.c
+++ src/lexer.c
@@ -34,40 +34,41 @@ Command tokenize(char *string)
 {
     size_t token_count = 0;
     size_t token_size = 4;
-    char ** token_array = malloc(sizeof(char *) * token_size);
-    if (token_array == NULL) 
+    Command token_struct;
+    token_struct.argv = malloc(sizeof(char *) * token_size);
+    if (token_struct.argv == NULL) 
     {
-        return NULL;
+        return (Command){0};
     } 
 
     char * token = strtok(string," \t"); // either space or tab
     while (token != NULL)
     {
-        token_array[token_count] = token;
+        token_struct.argv[token_count] = token;
         token = strtok(NULL," \t");
         token_count++;
 
         if ((token_count + 1) >= token_size)
         {
             token_size *= 2;
-            char **temp = realloc(token_array,sizeof(char*) * token_size);
+            char **temp = realloc(token_struct.argv,sizeof(char*) * token_size);
             if (temp == NULL) 
             {
-                free(token_array);
-                return NULL;
+                free(token_struct.argv);
+                return (Command){0};
             } 
-            else token_array = temp;
+            else token_struct.argv = temp;
         }
     }
 
-    char ** temp = realloc(token_array,sizeof(char*) * (token_count + 1) );
+    char ** temp = realloc(token_struct.argv,sizeof(char*) * (token_count + 1) );
     if (temp != NULL) 
     {
-        token_array = temp;
+        token_struct.argv = temp;
     }
     // Lets just return it as was  in case of a realloc fail without freeing
     // since this part only attempts to lower the size, we already got more than enough memory anyway 
 
-    token_array[token_count] = NULL;
-    return token_array;
+    token_struct.argv[token_count] = NULL;
+    return token_struct;
 }
