16e3fb0d1ecc356255df2cee6ffd771be6ae70a7
gitduck6 <warmsot19@gmail.com>
2026-05-11 16:57:17 +0400

    wrote some comments

--- src/runcmd.c
+++ src/runcmd.c
@@ -1,16 +1,42 @@
 #include "runcmd.h"
 
+/*
+    * In this documentation i will reference "sections" of the code with the following syntax:
+    * // SECTION NAME:
+    * ----
+    * i/o redirection
+    * If output not null, we assume its a filename
+    * since the tokenizer sets it to NULL If no file redirection involved
+    * and after assuming that its a filename we change out stdout to cmd.output
+    * with the freopen function.
+    * 
+    * Same with the input for stdin
+    * ----
+    * creating the process
+    * This shell follows a method common in unix systems:
+    * - create a child (a clone via fork() )
+    * - turn the child to a process we need (via execvp() )
+    * - and as the parent, wait till the child exits
+    * NOTE: return value of fork is:
+    * - 0 for the child
+    * - PID of the child for the parent
+    * - and a negative value in case of failiure
+*/
+
 int runcmd(Command cmd)
-{
+{    
+    // creating the process:
     __pid_t pid = fork();
 
     if (pid == 0)
     {
+        // i/o redirection:
         if (cmd.output != NULL)
             freopen(cmd.output,"w",stdout);
             
         if (cmd.input != NULL)
             freopen(cmd.input,"r",stdin);
+        
         
         execvp(cmd.argv[0],cmd.argv);
 
